a19260c80a970472a14c24be895a95596eb133d1
zjp <jiping_zhou@foxmail.com>
2023-02-26 13:12:18 +0800

    add: FN_POINTERS and FN_MAP statics

--- method-miner/src/lib.rs
+++ method-miner/src/lib.rs
@@ -1,30 +1,67 @@
 //!
 
+#![feature(type_name_of_val)]
+
+use std::collections::HashMap;
+
 use miner_macro::mine_methods;
 
-
-struct Foo;
+pub struct Foo;
 
 #[mine_methods] // <-- Generates a `foo_methods` module in the parent module
 impl Foo {
-    fn foo() -> u8 { 42 }
+    pub fn foo() {}
 
-    fn bar() {}
+    pub fn bar() {}
 }
 
+pub struct Bar;
+pub struct StructName;
 
+#[mine_methods] // <-- Generates a `bar_methods` module in the parent module
+impl Bar {
+    pub fn foo(_: &mut StructName, _: HashMap<String, String>) -> anyhow::Result<()> {
+        println!("called from Bar::foo");
+        Ok(())
+    }
+
+    pub fn bar(_: &mut StructName, _: HashMap<String, String>) -> anyhow::Result<()> {
+        println!("called from Bar::bar");
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use std::any::type_name_of_val;
 
     #[test]
-    fn it_works() {
-
-        println!("methods:");
+    fn test_foo() {
+        println!("methods in Foo:");
         for method in super::foo_methods::METHODS.iter() {
-            println!("  {method}");
+            println!("{method}");
         }
-        todo!()
+        for method in super::foo_methods::FN_POINTERS.iter() {
+            (method)(); // method call
+        }
+        for (fn_name, f) in super::foo_methods::FN_MAP.iter() {
+            println!("{fn_name} {}", type_name_of_val(f));
+        }
+    }
+
+    #[test]
+    fn test_bar() {
+        use super::{HashMap, StructName};
+        println!("methods in Bar:");
+        for method in super::bar_methods::METHODS.iter() {
+            println!("{method}");
+        }
+        let (arg1, arg2) = (&mut StructName, HashMap::new());
+        for method in super::bar_methods::FN_POINTERS.iter() {
+            (method)(arg1, arg2.clone()).unwrap(); // method call
+        }
+        for (fn_name, f) in super::bar_methods::FN_MAP.iter() {
+            println!("###{fn_name}### {}", type_name_of_val(f));
+        }
     }
 }
--- miner-macro/src/lib.rs
+++ miner-macro/src/lib.rs
@@ -2,11 +2,12 @@
 
 use proc_macro::TokenStream;
 use proc_macro2::{Span as Span2, TokenStream as TokenStream2};
-use syn::{Ident, Item, ImplItem, ImplItemMethod};
 use quote::quote;
+use syn::{FnArg, Ident, ImplItem, ImplItemMethod, Item, Type};
 
 #[proc_macro_attribute]
 pub fn mine_methods(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut output = item.clone(); // Haven't consider attr on the impl
     let item: TokenStream2 = item.into();
     let parsed_item = syn::parse2::<Item>(item).expect("Failed to parse item");
     if let Item::Impl(item_impl) = parsed_item {
@@ -15,29 +16,74 @@ pub fn mine_methods(_attr: TokenStream, item: TokenStream) -> TokenStream {
             "#[mine_methods] is only applicable to inherent impl blocks"
         );
 
-        let mod_name = format!("foo_methods"); // TODO change to e.g. <TYPE>_methods
+        let self_ty = if let Type::Path(implementor) = &*item_impl.self_ty {
+            implementor.path.get_ident().unwrap()
+        } else {
+            unimplemented!("implementor must be a single ident")
+        };
+        // struct name Foo -> mod prefix foo
+        let mod_name = format!("{}_methods", self_ty.to_string().to_ascii_lowercase());
         let mod_name = Ident::new(&mod_name, Span2::call_site());
 
+        // function pointer sig: fn(...) -> ...
+        let mut fn_sig: Option<TokenStream2> = None;
+        // function pointer: Foo::method
+        let mut fn_pointers: Vec<TokenStream2> = vec![];
+        // method name
         let mut methods: Vec<String> = vec![];
         for item in &item_impl.items {
             if let ImplItem::Method(ImplItemMethod { sig, .. }) = item {
                 methods.push(format!("{}", quote! { #sig }));
+                fn_sig.get_or_insert_with(|| fn_args(sig.inputs.iter(), &sig.output));
+                fn_pointers.push(fn_pointer(self_ty, &sig.ident));
             }
         }
 
+        println!(
+            "{}\n{}",
+            quote!( #fn_sig ),
+            fn_pointers
+                .iter()
+                .map(|f| f.to_string())
+                .collect::<Vec<_>>()
+                .join("\t")
+        );
         let quoted = quote! {
             pub mod #mod_name {
+                use super::*;
+                use std::collections::HashMap;
+                type FnPointer = #fn_sig;
                 lazy_static::lazy_static! {
                     pub static ref METHODS: Vec<&'static str> = vec![
                         #(#methods),*
                     ];
+                    pub static ref FN_POINTERS: Vec<  FnPointer  > = vec![
+                        #(#fn_pointers),*
+                    ];
+                    pub static ref FN_MAP: HashMap< &'static str, FnPointer > = {
+                        METHODS.iter().zip(FN_POINTERS.iter()).map(|(s, f)| (*s, *f)).collect()
+                    };
                 }
             }
         };
-        quoted.into()
-
+        output.extend(TokenStream::from(quoted));
+        output
     } else {
         panic!("#[mine_methods] is only applicable to inherent impl blocks")
     }
+}
 
+fn fn_pointer(self_ty: &Ident, method: &Ident) -> TokenStream2 {
+    quote!(#self_ty :: #method)
+}
+
+fn fn_args<'a>(inputs: impl Iterator<Item = &'a FnArg>, output: &syn::ReturnType) -> TokenStream2 {
+    let iter = inputs.filter_map(|i| {
+        if let FnArg::Typed(arg) = i {
+            Some(&*arg.ty)
+        } else {
+            None
+        }
+    });
+    quote!( fn ( #(#iter),* ) #output )
 }
